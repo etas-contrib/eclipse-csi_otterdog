f8d882fc7dde5bbfff80581eb63d393c9d208678
Wolfgang Fischer <wolfgang.fischer@etas.com>
2026-02-11 14:05:31 +0100

    fix:ignore missing library stubs or py.typed marker for jsonbender  to calm down mypy

--- otterdog/jsonnet.py
+++ otterdog/jsonnet.py
@@ -12,8 +12,8 @@ from functools import cached_property
 from shutil import ignore_patterns
 from typing import Any
 
-import aiofiles.os
-import aiofiles.ospath
+import aiofiles.os  # type: ignore
+import aiofiles.ospath  # type: ignore
 
 from .logging import get_logger
 from .utils import jsonnet_evaluate_snippet, parse_github_url, parse_template_url
@@ -351,7 +351,7 @@ class JsonnetConfig:
     async def jsonnet_template_files(self):
         import os
 
-        import aiofiles
+        import aiofiles  # type: ignore
 
         for file in await aiofiles.os.listdir(self.template_dir):
             if file.endswith(".libsonnet"):
